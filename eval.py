7fe219db7048cd069c40bc6b2dd6c632b402ad00
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-03-12 15:42:29 +1100

    Fully functional

---
+++ eval.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env fiji
+"""Compute the Rand Error between two segmentations.
+"""
+import argparse
+
+
+def evaluate(gt, auto):
+    """Run the main Fiji-based functionality.
+    """
+    from jython_imports import IJ, RandError
+    impgt = IJ.openImage(gt)
+    impauto = IJ.openImage(auto)
+    fscore = RandError.adaptedRandIndexFScore3D(impgt, impauto)
+    print(1.0 - fscore)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Do work using Fiji's huge plugin library.")
+    parser.add_argument('gt', help='The image containing the ground truth'
+                                   'segmentation.')
+    parser.add_argument('auto', help='The image containing the candidate'
+                                     'segmentation.')
+
+    args = parser.parse_args()
+    evaluate(args.gt, args.auto)
